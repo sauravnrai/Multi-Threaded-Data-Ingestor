e722eeda3b8c84143bcadcb0235716f221013bac
Saurav Narayan Rai <sauravnarayanrai88@gmail.com>
2025-02-10 11:19:56 +0530

    Initial commit

---
+++ springboot-data-ingestor/src/test/java/com/ingestor/springboot_data_ingestor/SpringbootDataIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingestor.springboot_data_ingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDataIngestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
